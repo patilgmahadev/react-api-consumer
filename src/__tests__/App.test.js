ef47e9819deba1e4fbb3dfd915ab321016ee0558
Mahadev Patil <venbo5@sg.uob>
2020-11-04 10:45:53 +0800

    feat(API Consumer) - updated unit test cases and Read me file

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Routes from "../components/Routes";
 
 describe("App Component", () => {
   it("renders App", () => {
     let wrapped = shallow(<App />);
     expect(wrapped).toBeDefined();
+  });
+
+  it("renders Header", () => {
+    let wrapped = shallow(<App />);
+    expect(wrapped.find(Header).exists()).toBe(true);
+  });
+
+  it("renders Footer", () => {
+    let wrapped = shallow(<App />);
+    expect(wrapped.find(Footer).exists()).toBe(true);
+  });
+
+  it("renders Routes", () => {
+    let wrapped = shallow(<App />);
+    expect(wrapped.find(Routes).exists()).toBe(true);
   });
 });
--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Home from "../components/Home";
+import Table from "../components/Table";
 
 describe("Home Component", () => {
   it("renders Home", () => {
     let wrapped = shallow(<Home />);
     expect(wrapped).toBeDefined();
+  });
+
+  it("renders Table", () => {
+    let wrapped = shallow(<Home />);
+    expect(wrapped.find(Table).exists()).toBe(true);
   });
 });
